4e2b982b0a480262a515972979671d1d17e29acd
Ettienne Scharneck <ettiennes@live.co.za>
2018-04-06 14:48:45 +0200

    Refactor shortcut to use function, add new link to SP app page

--- go.ps1
+++ go.ps1
@@ -3,6 +3,16 @@ if ($(Get-ExecutionPolicy) -ne "Unrestricted")
     Set-ExecutionPolicy Bypass -Force
 }
 
+function Create-Shortcut
+{
+    param( [string]$fileLocation, [string]$targetPath )
+
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($fileLocation)
+    $Shortcut.TargetPath = $targetPath
+    $Shortcut.Save()
+}
+
 $servers = @("sa-build01-v.k2workflow.com", 
              "sa-build02-v.k2workflow.com", 
              "sa-build03-v.k2workflow.com", 
@@ -19,11 +29,10 @@ if (!(Test-Path $folder))
 
 foreach ($server in $servers)
 {
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("$folder\$server.lnk")
-    $Shortcut.TargetPath = "\\$server\Builds\"
-    $Shortcut.Save()
+  Create-Shortcut "$folder\$server.lnk" "\\$server\Builds\"
 }
+
+Create-Shortcut "$folder\Add App to Sharepoint.lnk" "https://portal.denallix.com/sites/AppCatalog/_layouts/15/addanapp.aspx?Source=https%3A%2F%2Fportal%2Edenallix%2Ecom%2Fsites%2FAppCatalog%2F%5Flayouts%2F15%2Fviewlsts.aspx"
 
 Write-Host "Done!"
 Read-Host "Did it work?"
