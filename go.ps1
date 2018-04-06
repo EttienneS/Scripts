88d90b29f017eb34e36ccd9bfd3a3115b894eddf
Ettienne Scharneck <ettiennes@live.co.za>
2018-04-06 11:42:12 +0200

    add code to create links to all known servers

--- go.ps1
+++ go.ps1
@@ -1,2 +1,24 @@
-Write-Host "This is a test"
-Read-Host "Did it work?"
+$servers = @("sa-build01-v.k2workflow.com", 
+             "sa-build02-v.k2workflow.com", 
+             "sa-build03-v.k2workflow.com", 
+             "sa-build04-v.k2workflow.com", 
+             "sa-build05-v.k2workflow.com", 
+             "sa-build06-v.k2workflow.com")
+
+$folder = "$home\Desktop\Links"
+
+if (!(Test-Path $folder)) 
+{
+    New-Item $folder
+}   
+
+foreach ($server in $servers)
+{
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$folder\$server.lnk")
+    $Shortcut.TargetPath = "\\$server\Builds\"
+    $Shortcut.Save()
+}
+
+Write-Host "Done!"
+Read-Host "Press any key to exit."
