61d1f6b207bbbcc0bca2aabea3fa7de8367ca960
Ettienne Scharneck <ettiennes@live.co.za>
2018-04-11 08:10:37 +0200

    Add new shares

--- go.ps1
+++ go.ps1
@@ -13,13 +13,6 @@ function Add-Shortcut
     $Shortcut.Save()
 }
 
-$servers = @("sa-build01-v.k2workflow.com", 
-             "sa-build02-v.k2workflow.com", 
-             "sa-build03-v.k2workflow.com", 
-             "sa-build04-v.k2workflow.com", 
-             "sa-build05-v.k2workflow.com", 
-             "sa-build06-v.k2workflow.com")
-
 $folder = "$home\Desktop\Links\"
 
 if (Test-Path $folder) 
@@ -29,9 +22,20 @@ if (Test-Path $folder)
 
 New-Item $folder -ItemType Directory
 
+$servers = @("\\sa-build01-v.k2workflow.com\builds", 
+             "\\sa-build02-v.k2workflow.com\builds", 
+             "\\sa-build03-v.k2workflow.com\builds", 
+             "\\sa-build04-v.k2workflow.com\builds", 
+             "\\sa-build05-v.k2workflow.com\builds", 
+             "\\sa-build06-v.k2workflow.com\builds",
+             "\\sa-nas01-p.k2workflow.com\Appit Production Builds",
+             "\\scinstall.k2workflow.com\public",
+             "\\sa-nas01-p.k2workflow.com\Builds\K2 BlackPearl\Installer\00. Released Builds")
+
 foreach ($server in $servers)
 {
-    Add-Shortcut "$folder\$server.lnk" "\\$server\Builds\"
+    $machine = $server.Split('\\',[System.StringSplitOptions]::RemoveEmptyEntries)[0]
+    Add-Shortcut "$folder\$machine.lnk" "$server"
 }
 
 Add-Shortcut "$folder\Add App to Sharepoint.lnk" "https://portal.denallix.com/sites/AppCatalog/_layouts/15/addanapp.aspx?Source=https%3A%2F%2Fportal%2Edenallix%2Ecom%2Fsites%2FAppCatalog%2F%5Flayouts%2F15%2Fviewlsts.aspx"
